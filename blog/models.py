7a019aae09fee8a9686b28a536ad562705768b69
DanCavi <dante.cavieres@hotmail.com>
2023-12-07 17:49:17 -0300

    Primer push working

--- blog/models.py
+++ blog/models.py
@@ -4,10 +4,11 @@ import datetime
 # Create your models here.
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='blog/images')
     date = models.DateTimeField(default=datetime.date.today)
 
     def __str__(self) -> str:
         return self.title
+    
